8eebc4fe93d3777360183fac6f7a1765ced9e306
Christian Requerme <crequerme@academic.rrc.ca>
2024-03-21 14:08:09 -0500

    Test database and context manager added

--- db_operations.py
+++ db_operations.py
@@ -1,23 +1,96 @@
 """
-Description:
-Author:
-Section Number:
-Date Created:
+Description: Weather Data Table
+Author: Christian Requerme
+Section Number: FTO01
+Date Created: 03/21/24
 Credit:
 Updates:
 """
 
 import sqlite3
-conn = sqlite3.connect("weather.sqlite")
-print("Opened the database successfully.")
+from contextlib import contextmanager
 
-cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS weather_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    sample_date TEXT UNIQUE NOT NULL,
-    location TEXT NOT NULL,
-    min_temp REAL NOT NULL,
-    max_temp REAL NOT NULL,
-    avg_temp REAL NOT NULL,
-    UNIQUE (sample_date, location);""")
-print("Table created successfully.")
+class DBOperations:
+    """Class to perform database operations"""
+
+    def __init__(self, db_file):
+        """
+        Initialize DBOperations object.
+
+        Args:
+            db_file (str): The path to the SQLite database file.
+        """
+        self.db_file = db_file
+
+    def initialize_db(self):
+        """
+        Initializes the database.
+
+        This method creates the necessary table if it doesn't already exist.
+        """
+        with self._get_cursor() as cur:
+            cur.execute("""
+                        CREATE TABLE IF NOT EXISTS weather_data (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            sample_date TEXT NOT NULL,
+                            location TEXT NOT NULL,
+                            min_temp REAL NOT NULL,
+                            max_temp REAL NOT NULL,
+                            avg_temp REAL NOT NULL,
+                            UNIQUE (sample_date, location)
+                        );
+                        """)
+
+    def save_data(self, data):
+        """
+        Save new data to the database.
+
+        Args:
+            data (tuple): A tuple containing the data to be inserted.
+        """
+        with self._get_cursor() as cur:
+            cur.execute("SELECT id FROM weather_data WHERE sample_date = ? AND location = ?;",
+                         (data['sample_date'], data['location']))
+            existing_data = cur.fetchone()
+
+            if not existing_data:
+                cur.execute("""
+                            INSERT INTO weather_data (sample_date, location, min_temp, max_temp, avg_temp)
+                            VALUES (?, ?, ?, ?, ?);
+                            """, (data['sample_date'], data['location'], data['min_temp'],
+                                  data['max_temp'], data['avg_temp']))
+
+    def purge_data(self):
+        """
+        Purge all data from the database.
+        """
+        with self._get_cursor() as cur:
+            cur.execute("DELETE FROM weather_data;")
+
+    def fetch_data(self):
+        """
+        Fetch all data from the database.
+
+        Returns:
+            list: A list of tuples containing the fetched data.
+        """
+        with self._get_cursor() as cur:
+            cur.execute("SELECT * FROM weather_data;")
+            return cur.fetchall()
+
+    @contextmanager
+    def _get_cursor(self):
+        """
+        Context manager to get a cursor for executing SQL queries.
+
+        Yields:
+            cursor: A cursor object.
+        """
+        conn = sqlite3.connect(self.db_file)
+        try:
+            cur = conn.cursor()
+            yield cur
+        finally:
+            conn.commit()
+            cur.close()
+            conn.close()
---
+++ dbcm.py
@@ -0,0 +1,65 @@
+"""
+Description: DBCM
+Author: Christian Requerme
+Section Number: FTO01
+Date Created: 03/21/24
+Credit:
+Updates:
+"""
+
+
+import sqlite3
+from contextlib import ContextDecorator
+
+class DBCM(ContextDecorator):
+    """
+    A context manager for managing SQLite database connections.
+
+    Usage:
+    ```
+    with DBCM('database.db') as cursor:
+        cursor.execute("SELECT * FROM table_name")
+        result = cursor.fetchall()
+    ```
+
+    When exiting the context, the changes are committed if no exceptions occurred, otherwise,
+      they are rolled back.
+    """
+
+    def __init__(self, db_name):
+        """
+        Initialize the DBCM context manager.
+
+        Args:
+            db_name (str): The name of the SQLite database file.
+        """
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        """
+        Enter the context and establish a connection to the SQLite database.
+
+        Returns:
+            sqlite3.Cursor: A cursor object for executing SQL queries.
+        """
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Exit the context and commit or rollback changes to the database.
+
+        Args:
+            exc_type (type): The type of the exception that occurred, if any.
+            exc_value (Exception): The exception instance that occurred, if any.
+            traceback (traceback): The traceback object associated with the exception, if any.
+        """
+        if exc_type is not None:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
