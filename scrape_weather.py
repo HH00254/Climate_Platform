e379c0eaa15c73c1404745a1f955cc9ff2031ce4
HH00254 <105815210+HH00254@users.noreply.github.com>
2024-03-25 06:57:40 -0500

    updating my code

--- scrape_weather.py
+++ scrape_weather.py
@@ -14,16 +14,12 @@ Credit:
 Updates:
 
 """
-
 import requests
-
 from lxml import html
-
 from datetime import datetime
-
 from pprint import pprint
-
- 
+import calendar
+import re
 
 def format_payload_for_insert(list_data: list, location_payload: list, step: int) -> list[tuple]:
 
@@ -43,125 +39,99 @@ def format_payload_for_insert(list_data: list, location_payload: list, step: int
 
       need to be reformated into a dictionary data structure
 
- 
-
     Return:
 
     - Returns a dictionary
 
     '''
-
     for location_element in location_payload:
 
- 
-
         if location_element[0] == '' or location_element[0] == None:
-
             location_element = 'NULL'
-
- 
 
     insert_args = []
 
- 
-
     for index in range(0, len(list_data), step):
-
- 
-
         try:
-
             insert_args.append((
-
                 format_date(list_data[index]),
-
                 str(location_payload[0]).split(' ', maxsplit=1)[0],
-
                 str(location_payload[1]).strip(),
-
-                float(list_data[index + 1]),
-
-                float(list_data[index + 2]),
-
-                float(list_data[index + 3])))
-
- 
+                float(sanitize_string(list_data[index + 1])),
+                float(sanitize_string(list_data[index + 2])),
+                float(sanitize_string(list_data[index + 3]))))
 
         except TypeError as e:
-
             print(f'Error: {e}')
-
-           
-
-        
 
         except ValueError as e:
-
             print(f'Error: {e}')
-
-           
-
-            
 
     return insert_args
 
  
 
 def format_date(unformatted_date: str) -> str:
-
     return datetime.strptime(unformatted_date, '%B %d, %Y').strftime('%Y-%d-%m')
 
- 
+def sanitize_string(input_string):
+    # Use regular expression to find digits in the input string
+    sanitized_string = re.sub(r"[+,-,''][0-9]\.[0-9]", '', input_string).strip()
+
+    return sanitized_string
+
+def get_days_in_month(year, month) -> int:
+    # Get the number of days in the specified month
+    return calendar.monthrange(year, month)[1]
 
 def main()-> None:
-
     """
-
     Summary:
 
     - The main executable body for this module
 
     """
-
-    day   = datetime.now().day
     month = datetime.now().month
     year  = datetime.now().year
+    last_year = year + 1
+    decrement_value = 1
+    table_load = []
 
- 
+    for index in range(year):
+        if year != last_year:
+            while month != 0:
+                
+                request = f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year={year}&Month={month}#'
+                month = month - 1
+                
+                response_body = requests.get(request, timeout=60)
 
-    request = f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year={year}&Month={month}#'
+                if response_body.status_code == 200 and response_body.__sizeof__() > 0:
+                    tree = html.fromstring(response_body.content)
 
-    response_body = requests.get(request, timeout=60)
+                    city_path     = '//main/div/p/text()'
+                    province_path = '//main/div/br/text()'
+                    location_payload = tree.xpath(f"{city_path} | {province_path}")
 
- 
+                    number_of_days = get_days_in_month(year, month)
 
-    if response_body.status_code == 200 and response_body.__sizeof__() > 0:
+                    for inner_index in range(number_of_days):
+                        
+                        date_path       = f'//table/tbody/tr[{inner_index}]/th/abbr/@title'
+                        tempature_path  = f'//tr[{inner_index}]/td[position()<4]/text()'
+                    
+                        table_load.append(tree.xpath(f"{date_path} | {tempature_path}"))
 
-        tree = html.fromstring(response_body.content)
+                    # insert_values = format_payload_for_insert(table_load, location_payload, 4)
+                    
+                    pprint(table_load)
 
- 
-
-        city_path     = '//main/div/p/text()'
-        province_path = '//main/div/br/text()'
-        location_payload = tree.xpath(f"{city_path} | {province_path}")
-
-        date_path       = f'//table/tbody/tr[position() < {day}]/th/abbr/@title'
-        tempature_path  = f'//tr[position() < {day}]/td[position()<4]/text()'
-        td_tag_varients = f'//tr[position() < {day}]/td[position()<4]/a/text()'
-
-       
-
-        table_load = tree.xpath(f"{date_path} | {tempature_path} | {td_tag_varients}")
-
- 
-
-        insert_values = format_payload_for_insert(table_load, location_payload, 4)
-
- 
-
-        pprint(insert_values)
-
- 
+            month = 12
+            decrement_value = decrement_value + index
+            last_year = year
+            year = year - decrement_value
+        else:
+            break
 
 if __name__ == '__main__':
 
