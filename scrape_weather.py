f790787eb0dc7d327315ef8190dfbe87211f5bfd
Lance-Fuentes <lfuentes.tech@gmail.com>
2024-03-25 17:08:16 -0500

    Als Update

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,3 +1,4 @@
+
 """
 Description: Weather Processing App Group 9
 Author: Lance Fuentes, Al Hochbaum, Christian Requerme
@@ -10,68 +11,66 @@ import requests
 from lxml import html
 from datetime import datetime
 from pprint import pprint
-#pip install python-dateutil
+import calendar
 from dateutil.relativedelta import relativedelta
 
 def format_payload_for_insert(list_data: list, location_payload: list, step: int) -> list[tuple]:
+    '''
+    Summary:
+    - Copys a data-structure of type list and converts it into a
+        dictionary
+    ARGS:
+    - A list stucture containing key and values from a web-scrap that
+        need to be reformated into a dictionary data structure
+    Return:
+    - Returns a dictionary
+    '''
+    for location_element in location_payload:
+        if location_element[0] == '' or location_element[0] == None:
+            location_element = 'NULL'
+ 
     insert_args = []
-
     index = 0
     while index < len(list_data):
-        if not any(char.isdigit() for char in list_data[index]):
-            index += step  # Skip this date and move to the next one
-            continue
 
         try:
-            date = format_date(list_data[index])
-            location = str(location_payload[0]).split(' ', maxsplit=1)[0]
-            province = str(location_payload[1]).strip()
-            max_temp = try_convert_to_float(list_data[index + 1])
-            min_temp = try_convert_to_float(list_data[index + 2])
-            mean_temp = try_convert_to_float(list_data[index + 3])
-
-            if None in (max_temp, min_temp, mean_temp):
-                # If any temperature is None, skip this entry and move to the next one
-                index += 1
-                continue
-
-            insert_args.append((
-                date,
-                location,
-                province,
-                max_temp,
-                min_temp,
-                mean_temp))
-        except IndexError:
-            # Not enough elements in list_data
-            index += step
-            continue
-        except Exception as e:
+            if checking_for_date(list_data, index, step):
+                insert_args.append((
+                    format_date(list_data[index]),
+                    str(location_payload[0]).split(' ', maxsplit=1)[0],
+                    str(location_payload[1]).strip(),
+                    float(list_data[index + 1]),
+                    float(list_data[index + 2]),
+                    float(list_data[index + 3])))
+            else:
+                # Not enough elements in list_data
+                index =  index - step + 1
+        except TypeError as e:
             print(f'Error: {e}')
+        except ValueError as e:
+            print(f'Error: {e}')
+        except IndexError as e:
+            print(f'Error: {e}')
+        finally:
+            # Continue Incrementation
             index += step
-            continue
-
-        index += step
 
     return insert_args
 
-
-def try_convert_to_float(value: str) -> float:
-    try:
-        return float(value)
-    except (ValueError, TypeError):
-        return None
-
 def format_date(unformatted_date: str) -> str:
-    try:
-        # Attempt to parse the date in various formats
-        formatted_date = datetime.strptime(unformatted_date, '%B %d, %Y').strftime('%Y-%m-%d')
-    except ValueError:
-        try:
-            formatted_date = datetime.strptime(unformatted_date, '%Y-%m-%d').strftime('%Y-%m-%d')
-        except ValueError:
-            formatted_date = ''
-    return formatted_date
+    return datetime.strptime(unformatted_date, '%B %d, %Y').strftime('%Y-%m-%d')
+
+def checking_for_date(data_collection, index, step) -> bool:
+    error_flag = False
+
+    if len(data_collection) == index + step:
+        error_flag = True
+    elif index + step < len(data_collection):
+        if(str(data_collection[index]).split(' ', maxsplit=1)[0].strip() ==
+           str(data_collection[index + step]).split(' ', maxsplit=1)[0].strip()):
+            error_flag = True
+
+    return error_flag
 
 
 def main()-> None:
@@ -79,11 +78,12 @@ def main()-> None:
     Summary:
     - The main executable body for this module
     """
+    current_date   = datetime.now()
+    previous_data  = None
+    data_flag = True
 
-    current_date = datetime.now()
-    previous_data = None
+    while data_flag:
 
-    while True:
         month = current_date.month
         year  = current_date.year
 
@@ -97,24 +97,25 @@ def main()-> None:
             province_path = '//main/div/br/text()'
             location_payload = tree.xpath(f"{city_path} | {province_path}")
 
-            date_path      = '//table/tbody/tr/th/abbr/@title'
-            temperature_path = '//tr/td[position()<4]/text()'
+            date_path      = '//table/tbody/tr[position()< last() -3]/th/abbr/@title'
+            temperature_path = '//tr[position()< last() -3]/td[position()<4]/text()'
             table_load = tree.xpath(f"{date_path} | {temperature_path}")
 
-            # pprint(table_load)
             # Check if the current data is not empty and is the same as the previous non-empty data
             if table_load and table_load == previous_data:
-                break
+                data_flag = False
+
             elif table_load:
                 previous_data = table_load
+                insert_values = format_payload_for_insert(table_load, location_payload, 4)
 
-            insert_values = format_payload_for_insert(table_load, location_payload, 4)
-
-            pprint(insert_values)
+                pprint(insert_values)
 
             #insert to databse code here
         else:
-            break
+            data_flag = False
+
+        # Decrementing the time frame.
         current_date -= relativedelta(months=1)
 
 if __name__ == '__main__':
