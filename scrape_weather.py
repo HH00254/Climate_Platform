69ee5c2410e78fd16773dfa43806a21909499433
HH00254 <105815210+HH00254@users.noreply.github.com>
2024-03-26 12:05:42 -0500

    Final version

--- scrape_weather.py
+++ scrape_weather.py
@@ -21,18 +21,18 @@ from pprint import pprint
 import calendar
 from dateutil.relativedelta import relativedelta
 
+
 def format_payload_for_insert(list_data: list, location_payload: list, step: int) -> list[tuple]:
     '''
     Summary:
-    - Copys a data-structure of type list and converts it into a
+    - Formats scraped data from the website
 
       dictionary
     ARGS:
     - A list stucture containing key and values from a web-scrap that
-
       need to be reformated into a dictionary data structure
     Return:
-    - Returns a dictionary
+    - Returns a List of tuples
 
     '''
     for location_element in location_payload:
@@ -60,13 +60,35 @@ def format_payload_for_insert(list_data: list, location_payload: list, step: int
                 index =  index - step + 1
 
         except TypeError as e:
-            print(f'Error: {e}')
+            system_log(e, 
+                       (
+                        list_data[index], 
+                        location_payload[0], 
+                        location_payload[1], 
+                        list_data[index + 1], 
+                        list_data[index + 2], 
+                        list_data[index + 3]))
 
-        except ValueError as e:
-            print(f'Error: {e}')
+        except ValueError as e:          
+            system_log(e, 
+                       (
+                        list_data[index], 
+                        location_payload[0], 
+                        location_payload[1], 
+                        list_data[index + 1], 
+                        list_data[index + 2], 
+                        list_data[index + 3]))
+
 
         except IndexError as e:
-            print(f'Error: {e}')
+            system_log(e, 
+                       (
+                        list_data[index], 
+                        location_payload[0], 
+                        location_payload[1], 
+                        list_data[index + 1], 
+                        list_data[index + 2], 
+                        list_data[index + 3]))
 
         finally:
             # Continue Incrementation
@@ -74,10 +96,54 @@ def format_payload_for_insert(list_data: list, location_payload: list, step: int
 
     return insert_args
 
+
+def system_log(exception: Exception, data_entre=None) -> None:
+    """
+    Summary:
+    - Opens and writes error logs to a file
+      when issues arise from web-scrapping
+
+    Args:
+    - Exception object from the event
+    - A data object of the corrupted data.
+
+    Return:
+    - None
+    """
+    log_date = datetime.now().strftime('%Y-%m-%d')
+    print('here!!')
+    with open(f'//{__name__}_{__package__}_{log_date}.txt' 'w', encoding='utf-8') as file_stream_output:
+
+        file_stream_output.write(f'Error: {exception.__cause__}\n')
+        file_stream_output.write(f'Data Row Corruption:\n {data_entre}\n')
+
+
 def format_date(unformatted_date: str) -> str:
+    """
+    Summary:
+    - Re-formates string date data
+
+    Args:
+    - An unformated date as a string
+
+    Return:
+    - A formated date
+    """
     return datetime.strptime(unformatted_date, '%B %d, %Y').strftime('%Y-%m-%d')
 
+
 def checking_for_date(data_collection, index, step) -> bool:
+    """
+    Check if the date at the current index is the same as the date at index + step in the data collection.
+
+    Args:
+    - data_collection (list): A list containing the data collection.
+    - index (int): The current index in the data collection.
+    - step (int): The step size to check for the next date.
+
+    Returns:
+    - bool: True if the dates are the same or if the index + step is at the end of the collection, False otherwise.
+    """
     error_flag = False
 
     if len(data_collection) == index + step:
@@ -92,53 +158,66 @@ def checking_for_date(data_collection, index, step) -> bool:
     return error_flag
 
 
-def main()-> None:
+def web_scrape_call()-> list[tuple]:
     """
     Summary:
+    - When invoked this method calls climate.weather.gc.ca
+      to begin scraping the website for weather data.
 
-    - The main executable body for this module
+    Args:
+    - None
 
+    Return:
+    - A list object containing tuples, that act as a row to store 
+      the scraped data for insertion into a DB.
     """
+    insert_values  = []
     current_date   = datetime.now()
     previous_data  = None
-    data_flag = True
+    call_attempt   = 0
+    data_flag      = True
 
-    while data_flag:
+    while data_flag and call_attempt < 12:
 
         month = current_date.month
         year  = current_date.year
 
         request = f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Year={year}&Month={month}#'
-        response_body = requests.get(request, timeout=60)
+        response_body = requests.get(request, timeout=120)
 
         if response_body.status_code == 200 and response_body.content:
             tree = html.fromstring(response_body.content)
 
-            city_path     = '//main/div/p/text()'
-            province_path = '//main/div/br/text()'
-            location_payload = tree.xpath(f"{city_path} | {province_path}")
+            if tree != previous_data:
+                previous_data = tree
 
-            date_path      = '//table/tbody/tr[position()< last() -3]/th/abbr/@title'
-            temperature_path = '//tr[position()< last() -3]/td[position()<4]/text()'
-            table_load = tree.xpath(f"{date_path} | {temperature_path}")
+                city_path     = '//main/div/p/text()'
+                province_path = '//main/div/br/text()'
+                location_payload = tree.xpath(f"{city_path} | {province_path}")
 
-            # Check if the current data is not empty and is the same as the previous non-empty data
-            if table_load and table_load == previous_data:
+                date_path      = '//table/tbody/tr[position()< last() -3]/th/abbr/@title'
+                temperature_path = '//tr[position()< last() -3]/td[position()<4]/text()'
+                table_load = tree.xpath(f"{date_path} | {temperature_path}")
+
+                insert_values.append(format_payload_for_insert(table_load, location_payload, 4))
+                call_attempt = 0
+
+            else:
                 data_flag = False
 
-            elif table_load:
-                previous_data = table_load
-                insert_values = format_payload_for_insert(table_load, location_payload, 4)
-
-                pprint(insert_values)
-
-            #insert to databse code here
         else:
-            data_flag = False
+            call_attempt +=  1
 
         # Decrementing the time frame.
         current_date -= relativedelta(months=1)
 
+    return insert_values
+
 if __name__ == '__main__':
-    main()
+    #for testing    
+    wheather_data =  web_scrape_call()
+
+    for weather_element in wheather_data:
+        pprint(weather_element)
+
     input('Press Enter to exit program...\n')
