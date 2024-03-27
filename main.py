eca6532df586ac6b2bd39ebaa8004bb628c341f7
HH00254 <105815210+HH00254@users.noreply.github.com>
2024-03-27 15:10:07 -0500

    final version of main

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from pprint import pprint
 from time import time
 
 def main() -> None:
+    """
+    Summary:
+    - Main body were we execute our code.
+    """
     db_operations = DBOperations('weather_data.sqlite')
-    now = time()
     insert_values = scrape_weather.web_scrape_call()
-    print('then')
-    then = time()
-    print(f'It took {now - then}')
-
-    pprint(insert_values)
 
     for entry in insert_values:
         db_operations.save_data(entry)
