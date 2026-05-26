9a3218078f3c8e426b730fe730c65c1a318943d1
HH00254 <HH00254@gmail.com>
2026-05-26 13:44:31 -0500

    working on serializers

--- backend/weather/api/serializers.py
+++ backend/weather/api/serializers.py
@@ -1,6 +1,53 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 
-# from weather.models import (
-#     Location,
-#     WeatherRecord
-# )
+from weather.model_definitions.location import Location  # pyright: ignore[reportMissingImports]
+
+from weather.model_definitions.weather_record import WeatherRecord  # pyright: ignore[reportMissingImports]
+
+class LocationSerializer(serializers.ModelSerializer):
+    """
+    Summary:
+    - Serializer for Location model.
+    """
+
+    class Meta:
+        """
+        Summary:
+        - Configuration for Location serializer.
+        """
+
+        model = Location
+
+        fields = [
+            "id",
+            "city",
+            "province",
+            "station_id"
+        ]
+
+class WeatherRecordSerializer(serializers.ModelSerializer):
+    """
+    Summary:
+    - Serializer for WeatherRecord model.
+    """
+
+    location = LocationSerializer(
+        read_only=True
+    )
+
+    class Meta:
+        """
+        Summary:
+        - Configuration for WeatherRecord serializer.
+        """
+
+        model = WeatherRecord
+
+        fields = [
+            "id",
+            "sample_date",
+            "location",
+            "min_temp",
+            "max_temp",
+            "avg_temp"
+        ]
